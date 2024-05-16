054a893d1add7c9f2fc482e2df2a5df4397f5bf6
NatalySv <136372155+NatalySv@users.noreply.github.com>
2024-05-16 23:07:53 +0300

    Add function createMarkup

--- src/main.js
+++ src/main.js
@@ -62,6 +62,38 @@ function getSearch() {
     .catch(error => console.log('catch', error));
 }
 
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({
+        id,
+        tags,
+        largeImageURL,
+        webformatURL,
+        comments,
+        likes,
+        downloads,
+        views,
+      }) =>
+        `<li id="${id}" class="gallery-item">
+            <a class="gallery-link" href="${largeImageURL}">
+                <img
+                    class="gallery-image"
+                    src="${webformatURL}"
+                    alt="${tags}"
+                    />
+            </a>
+            <ul class="gallery-stat-list">
+                <li class="stat-item"><h2 class="title">Likes</h2><p class="stat-data">${likes}</p></li>
+                <li class="stat-item"><h2 class="title">Views</h2><p class="stat-data">${views}</p></li>
+                <li class="stat-item"><h2 class="title">Comments</h2><p class="stat-data">${comments}</p></li>
+                <li class="stat-item"><h2 class="title">Downloads</h2><p class="stat-data">${downloads}</p></li>
+            </ul>
+        </li>`
+    )
+    .join('');
+}
+
 const iziOptions = {
   id: 'myIziToast',
   title: 'Error',
