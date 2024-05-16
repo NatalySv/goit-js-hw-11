fd07e72c5783f80e37c8557a077b5376f7fc501c
NatalySv <136372155+NatalySv@users.noreply.github.com>
2024-05-16 21:47:47 +0300

    Add check querySearch

--- src/main.js
+++ src/main.js
@@ -3,11 +3,16 @@ const refs = {
   input: document.querySelector('.form-input'),
   list: document.querySelector('.gallery'),
 };
-
+let querySearch = null;
 refs.form.addEventListener('submit', onSubmit);
 
 function onSubmit(event) {
   event.preventDefault();
-  console.log(refs.input.value);
+  console.log(`input before: `, querySearch);
+  if (!refs.input.value) {
+    return;
+  }
+  querySearch = refs.input.value;
+  console.log(`input after: `, querySearch);
   refs.form.reset();
 }
