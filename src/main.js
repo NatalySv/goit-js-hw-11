42e2c4fd010cb6735886920f26471c081becd393
NatalySv <136372155+NatalySv@users.noreply.github.com>
2024-05-16 22:00:38 +0300

    Add iziToast

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
 const refs = {
   form: document.querySelector('.task-form'),
   input: document.querySelector('.form-input'),
@@ -10,9 +13,25 @@ function onSubmit(event) {
   event.preventDefault();
   console.log(`input before: `, querySearch);
   if (!refs.input.value) {
+    iziToast.error({
+      ...iziOptions,
+      message: `Please enter your search query, the field cannot be blank!`,
+      backgroundColor: 'rgb(239, 64, 64)',
+      iconUrl: iconError,
+    });
     return;
   }
   querySearch = refs.input.value;
   console.log(`input after: `, querySearch);
   refs.form.reset();
 }
+
+const iziOptions = {
+  id: 'myIziToast',
+  title: 'Error',
+  titleColor: 'rgb(255, 255, 255)',
+  titleSize: '16',
+  messageColor: 'rgb(255, 255, 255)',
+  messageSize: '16',
+  position: 'topRight',
+};
