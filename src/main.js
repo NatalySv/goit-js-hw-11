56b4cd65da82f099c8dc5e48278aa6991e4faca4
NatalySv <136372155+NatalySv@users.noreply.github.com>
2024-05-16 22:53:50 +0300

    Add function getSearch

--- src/main.js
+++ src/main.js
@@ -7,7 +7,9 @@ const refs = {
   input: document.querySelector('.form-input'),
   list: document.querySelector('.gallery'),
 };
+
 let querySearch = null;
+
 refs.form.addEventListener('submit', onSubmit);
 
 function onSubmit(event) {
@@ -24,7 +26,40 @@ function onSubmit(event) {
   }
   querySearch = refs.input.value;
   console.log(`input after: `, querySearch);
+  getSearch();
   refs.form.reset();
+}
+
+function getSearch() {
+  const ApiKey = '43688767-8e78f2c96043da1155d4d6687';
+  const searchParams = new URLSearchParams({
+    key: ApiKey,
+    q: querySearch,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  });
+  console.log(`searchParams: ${searchParams.toString()}`);
+  return fetch(`https://pixabay.com/api/?${searchParams}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (!data.hits.length) {
+        iziToast.error({
+          ...iziOptions,
+          message: `Sorry, there are no images matching your search query. Please try again!`,
+          backgroundColor: 'rgb(239, 64, 64)',
+          iconUrl: iconError,
+        });
+        return;
+      }
+      refs.list.innerHTML = createMarkup(data.hits);
+    })
+    .catch(error => console.log('catch', error));
 }
 
 const iziOptions = {
